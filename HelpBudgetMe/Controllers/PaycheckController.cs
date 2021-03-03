ac959e33f0de17c28b6c68ba3aa327554eab9794
Patrick Carter <49718153+Patrick-Carter@users.noreply.github.com>
2021-03-03 00:59:29 -0600

    Added CRUD for paychecks

--- HelpBudgetMe/Controllers/PaycheckController.cs
+++ HelpBudgetMe/Controllers/PaycheckController.cs
@@ -1,5 +1,6 @@
 ﻿using HelpBudgetMe.Data;
 using HelpBudgetMe.Models;
+using HelpBudgetMe.Models.ViewModels.PaycheckVM;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -21,12 +22,35 @@ namespace HelpBudgetMe.Controllers
             _db = db;
             _userManager = userManager;
         }
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
+        [HttpGet]
 
+        public IActionResult AddPaycheck()
+        {
+            return View();
+        }
+        [HttpGet]
         public IActionResult EditPaycheck(int Id)
+        {
+            Paycheck paycheck = _db.Paychecks.Where(a => a.Id == Id).FirstOrDefault();
+
+            var model = new EditPaycheckViewModel()
+            {
+                Id = paycheck.Id,
+                Name = paycheck.Name,
+                Amount = paycheck.Amount,
+                PreviousAmount = paycheck.Amount,
+                DateCreated = paycheck.DateCreated
+            };
+
+            return View(model);
+        }
+
+        public IActionResult DeletePaycheck(int Id)
         {
 
             Paycheck paycheck = _db.Paychecks.Where(a => a.Id == Id).FirstOrDefault();
@@ -35,6 +59,38 @@ namespace HelpBudgetMe.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public async Task<IActionResult> DeletePaycheck(Paycheck paycheck)
+        {
+            if (ModelState.IsValid)
+            {
+                try { 
+                    string UserId = _userManager.GetUserId(User);
+                    User currentUser = _db.Users.Where(a => a.Id == UserId).FirstOrDefault();
+
+                    currentUser.CurrentMoney -= paycheck.Amount;
+                    currentUser.BudgetedForNeeds = currentUser.CurrentMoney * .5m;
+                    currentUser.BudgetedForWants = currentUser.CurrentMoney * .3m;
+                    currentUser.BudgetedForSavings = currentUser.CurrentMoney * .2m;
+                    currentUser.AllTimeEarned -= paycheck.Amount;
+                    await _userManager.UpdateAsync(currentUser);
+                    _db.Paychecks.Remove(paycheck);
+                    _db.SaveChanges();
+
+
+                    return RedirectToAction("Index", "Dashboard");
+                }
+                catch
+                {
+                    return BadRequest();
+                }
+            }
+            return BadRequest();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
         public async Task<IActionResult> AddPaycheck(Paycheck model)
         {
 
@@ -64,7 +120,7 @@ namespace HelpBudgetMe.Controllers
                     await _userManager.UpdateAsync(currentUser);
                     await _db.SaveChangesAsync();
 
-                    return RedirectToAction("Index", "Paycheck");
+                    return RedirectToAction("Index", "Dashboard");
                 }
                 catch
                 {
@@ -79,13 +135,44 @@ namespace HelpBudgetMe.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult EditPaycheck(Paycheck paycheck)
+        public async Task<IActionResult> EditPaycheck(EditPaycheckViewModel model)
         {
+            if (ModelState.IsValid) { 
+                try { 
+                    string Id = _userManager.GetUserId(User);
+                    User currentUser = _db.Users.Where(a => a.Id == Id).FirstOrDefault();
 
-            _db.Update(paycheck);
-            _db.SaveChanges();
+                    var paycheck = new Paycheck()
+                    {
+                        Id = model.Id,
+                        Name = model.Name,
+                        Amount = model.Amount,
+                        DateCreated = model.DateCreated,
+                        User = currentUser
+                    };
 
-            return RedirectToAction("Index", "Dashboard");
+                    decimal amountChanged = model.PreviousAmount - model.Amount;
+
+                    currentUser.CurrentMoney -= amountChanged;
+                    currentUser.BudgetedForNeeds = currentUser.CurrentMoney * .5m;
+                    currentUser.BudgetedForWants = currentUser.CurrentMoney * .3m;
+                    currentUser.BudgetedForSavings = currentUser.CurrentMoney * .2m;
+                    currentUser.AllTimeEarned -= amountChanged;
+
+                    await _userManager.UpdateAsync(currentUser);
+                    _db.Update(paycheck);
+                    _db.SaveChanges();
+
+                    return RedirectToAction("Index", "Dashboard");
+                }
+                catch { return BadRequest(); }
+            }
+            else
+            {
+                return View(model);
+            }
+
+            
         }
     }
 }
---
+++ HelpBudgetMe/Models/ViewModels/PaycheckVM/EditPaycheckViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelpBudgetMe.Models.ViewModels.PaycheckVM
+{
+    public class EditPaycheckViewModel
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(40)]
+        public string Name { get; set; }
+        [Required]
+        [Column(TypeName = "decimal(12,2)")]
+        [DataType(DataType.Currency)]
+        public decimal Amount { get; set; }
+        [Required]
+        [Column(TypeName = "decimal(12,2)")]
+        [DataType(DataType.Currency)]
+        public decimal PreviousAmount { get; set; }
+
+        [Required]
+        [DataType(DataType.DateTime)]
+        public DateTime DateCreated { get; set; }
+    }
+}
