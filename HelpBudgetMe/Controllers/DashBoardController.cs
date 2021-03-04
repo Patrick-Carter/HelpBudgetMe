8f3774a49dcbe8c32bc05d3c936a0fe02770355e
Patrick Carter <49718153+Patrick-Carter@users.noreply.github.com>
2021-03-04 17:44:24 -0600

    Limited query to 5 items on dash board

--- HelpBudgetMe/Controllers/DashBoardController.cs
+++ HelpBudgetMe/Controllers/DashBoardController.cs
@@ -27,37 +27,47 @@ namespace HelpBudgetMe.Controllers
        
         public async Task<IActionResult> Index()
         {
-            User user;
+            
             
             try {
                 string Id = _userManager.GetUserId(User);
-                user = await _db.Users
-                .Include(a => a.Needs).Take(10)
-                .Include(a => a.Wants).Take(10)
-                .Include(a => a.Savings).Take(10)
-                .Include(a => a.Paychecks).Take(10)
-                .Where(a => a.Id == Id)
-                .FirstOrDefaultAsync();
+
+                var user = await _db.Users.Where(a => a.Id == Id).Select(b => new User
+                {
+                    Paychecks = b.Paychecks.OrderByDescending(a => a.DateCreated).Take(5).ToList(),
+                    Needs = b.Needs.OrderByDescending(a => a.DateCreated).Take(5).ToList(),
+                    Wants = b.Wants.OrderByDescending(a => a.DateCreated).Take(5).ToList(),
+                    Savings = b.Savings.OrderByDescending(a => a.DateCreated).Take(5).ToList(),
+                    BudgetedForNeeds = b.BudgetedForNeeds,
+                    BudgetedForWants = b.BudgetedForWants,
+                    BudgetedForSavings = b.BudgetedForSavings,
+                    AllTimeEarned = b.AllTimeEarned,
+                    AllTimeSpent = b.AllTimeSpent,
+                    CurrentMoney = b.CurrentMoney
+                }).FirstOrDefaultAsync();
+
+                DashboardViewModel model = new DashboardViewModel();
+
+                model.Needs = user.Needs;
+                model.Wants = user.Wants;
+                model.Savings = user.Savings;
+                model.Paychecks = user.Paychecks;
+                model.NeedsBudget = user.BudgetedForNeeds;
+                model.WantsBudget = user.BudgetedForWants;
+                model.SavingsBudget = user.BudgetedForSavings;
+                model.AllTimeEarned = user.AllTimeEarned;
+                model.AllTimeSpent = user.AllTimeSpent;
+                model.CurrentMoney = user.CurrentMoney;
+
+                return View(model);
+
             }
             catch
             {
                 return BadRequest();
             }
 
-            DashboardViewModel model = new DashboardViewModel();
            
-            model.Needs = user.Needs;
-            model.Wants = user.Wants;
-            model.Savings = user.Savings;
-            model.Paychecks = user.Paychecks;
-            model.NeedsBudget = user.BudgetedForNeeds;
-            model.WantsBudget = user.BudgetedForWants;
-            model.SavingsBudget = user.BudgetedForSavings;
-            model.AllTimeEarned = user.AllTimeEarned;
-            model.AllTimeSpent = user.AllTimeSpent;
-            model.CurrentMoney = user.CurrentMoney;
-
-            return  View(model);
         }
     }
 }
