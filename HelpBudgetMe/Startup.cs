680104c610c37aaede5f60eff954de843f20cfba
Patrick Carter <patrickdcarterreal@gmail.com>
2023-06-10 16:58:26 -0500

    only https redirect in dev

--- HelpBudgetMe/Startup.cs
+++ HelpBudgetMe/Startup.cs
@@ -64,15 +64,15 @@ namespace HelpBudgetMe
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseHttpsRedirection();
             }
             else
             {
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                //app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
